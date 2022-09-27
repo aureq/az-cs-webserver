2e4f142579ac88047f327a1cdb81e6cdd24b3817
Aurelien Requiem <aurelien@requiem.fr>
2022-09-27 15:20:38 +1000

    ✨ convert our pulumi app so it uses our component resource

--- WebServerStack.cs
+++ WebServerStack.cs
@@ -1,9 +1,6 @@
 using Pulumi;
-using Pulumi.Azure.Compute;
-using Pulumi.Azure.Compute.Inputs;
 using Pulumi.Azure.Core;
-using Pulumi.Azure.Network;
-using Pulumi.Azure.Network.Inputs;
+using Pulumi.Azure.Storage;
 
 class WebServerStack : Stack
 {
@@ -13,92 +10,38 @@ class WebServerStack : Stack
 
         var resourceGroup = new ResourceGroup("webserver-demo-rg");
 
-        var network = new VirtualNetwork("server-network",
-            new VirtualNetworkArgs
+        // Create an Azure Storage Account
+        var storageAccount = new Account("storage",
+            new AccountArgs
             {
                 ResourceGroupName = resourceGroup.Name,
-                AddressSpaces = {"10.0.0.0/16"},
-                Subnets =
-                {
-                    new VirtualNetworkSubnetArgs {Name = "default", AddressPrefix = "10.0.1.0/24"}
-                }
-            }
-        );
-
-        var publicIp = new PublicIp("server-ip",
-            new PublicIpArgs
+                AccountReplicationType = "LRS",
+                AccountTier = "Standard",
+                // Tags = new InputMap<string>
+                // {
+                //     {"ProjectName", "AcmeProject"}
+                // },
+            },
+            new CustomResourceOptions
             {
-                ResourceGroupName = resourceGroup.Name,
-                AllocationMethod = "Dynamic"
+                Parent = resourceGroup
             });
 
-        var networkInterface = new NetworkInterface("server-nic",
-            new NetworkInterfaceArgs
+        var webServer = new WebServerComponent("webserver-component",
+            new WebServerComponentArgs
             {
-                ResourceGroupName = resourceGroup.Name,
-                IpConfigurations =
-                {
-                    new NetworkInterfaceIpConfigurationArgs
-                    {
-                        Name = "webserveripcfg",
-                        SubnetId = network.Subnets.Apply(subnets => subnets[0].Id)!,
-                        PrivateIpAddressAllocation = "Dynamic",
-                        PublicIpAddressId = publicIp.Id
-                    }
-                }
+                WebServerResourceGroup = resourceGroup,
+                ReportStorageAccount = storageAccount,
+                AdminUser = "AcmeAdmin",
+                AdminPassword = "HardcodedPasswords!AreBad4YourH3alth#",
+                // AdminPassword = stackConfig.RequireSecret("adminpassword"),
+            },
+            new ComponentResourceOptions {
+                Parent = resourceGroup
             });
 
-        var vm = new VirtualMachine("server-vm",
-            new VirtualMachineArgs
-            {
-                ResourceGroupName = resourceGroup.Name,
-                NetworkInterfaceIds = {networkInterface.Id},
-                VmSize = "Standard_A1_v2",
-                DeleteDataDisksOnTermination = true,
-                DeleteOsDiskOnTermination = true,
-                OsProfile = new VirtualMachineOsProfileArgs
-                {
-                    ComputerName = "hostname",
-                    AdminUsername = "testadmin",
-                    AdminPassword = stackConfig.Require("adminpassword"),
-                    // AdminPassword = "HardCodedPassword1234!isBad",
-                    CustomData =
-                        @"#!/bin/bash
-echo ""Hello, World!"" > index.html
-nohup python -m SimpleHTTPServer 80 &"
-                },
-                OsProfileLinuxConfig = new VirtualMachineOsProfileLinuxConfigArgs
-                {
-                    DisablePasswordAuthentication = false
-                },
-                StorageOsDisk = new VirtualMachineStorageOsDiskArgs
-                {
-                    CreateOption = "FromImage",
-                    Name = "myosdisk1"
-                },
-                StorageImageReference = new VirtualMachineStorageImageReferenceArgs
-                {
-                    Publisher = "canonical",
-                    Offer = "UbuntuServer",
-                    Sku = "16.04-LTS",
-                    Version = "latest"
-                }
-            }, new CustomResourceOptions {DeleteBeforeReplace = true});
-
-
-        // The public IP address is not allocated until the VM is running, so wait for that
-        // resource to create, and then lookup the IP address again to report its public IP.
-        this.IpAddress = Output
-            .Tuple<string, string, string>(vm.Id, publicIp.Name, resourceGroup.Name)
-            .Apply<string>(async t =>
-            {
-                (_, string name, string resourceGroupName) = t;
-                var ip = await GetPublicIP.InvokeAsync(new GetPublicIPArgs
-                    {Name = name, ResourceGroupName = resourceGroupName});
-                return ip.IpAddress;
-            });
-
-        this.WgetCommand = this.IpAddress.Apply(ip => "Run 'wget -q -O - -S http://" + ip + "' to test your web server.");
+        this.IpAddress = webServer.IpAddress;
+        this.WgetCommand = webServer.WgetCommand;
     }
 
     [Output] public Output<string> IpAddress { get; set; }
